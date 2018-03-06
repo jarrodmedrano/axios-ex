09b9f190e73b6af57ea2e007e0220a3b169e7bf2
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook-Pro.local>
2018-03-05 22:13:36 -0700

    adding featured images

--- src/App.js
+++ src/App.js
@@ -1,29 +1,56 @@
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import Slider from "react-viewport-slider";
+
+const url = "https://salty-wordpress.herokuapp.com/wp-json/wp/v2/";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-     serverResponse: ''
-    }
-   }
-
-   async getData(){
-    const res = await axios('https://jsonplaceholder.typicode.com/posts/1');
-    const data = await res.data;
-    this.setState({serverResponse: data})
+      serverResponse: [],
+      featuredImages: []
+    };
   }
+
+  async getData() {
+    const res = await axios(`${url}posts?_embed`);
+    const data = await res.data;
+    this.setState({ serverResponse: data });
+  }
+
+  async getFeaturedImg(id) {}
+
   componentDidMount() {
     this.getData().catch(alert);
   }
-  render(){
-   return(
-      <div>
-        {this.state.serverResponse.title}
-      </div>
-   );
+
+  render() {
+    return (
+      <Slider>
+        {this.state.serverResponse.map(function(item, id) {
+          return (
+            <Slider.Item
+              className="has-overlay"
+              style={{
+                backgroundImage: `url(${
+                  item._embedded["wp:featuredmedia"][0]["source_url"]
+                })`,
+                backgroundSize: "cover"
+              }}
+            >
+              <div className="content">{item.title.rendered}</div>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: item.excerpt.rendered
+                }}
+              />
+            </Slider.Item>
+          );
+        }, this)}
+      </Slider>
+    );
   }
 }
 
